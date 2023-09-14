30275945232c4917430bfafd38e6932edc2fe52e
GreatCode1989 <wef666wef6@gmail.com>
2023-09-14 13:05:58 +0300

    lo

--- src/components/Table/BaseTable.vue
+++ src/components/Table/BaseTable.vue
@@ -1,11 +1,56 @@
 <template>
   <div class="table-wrapper">
     <div class="table">
-      <div class="table-head"></div>
+      <div class="table-head" :style="{ 'grid-template-columns': columnTemplates }">
+        <div class="table-head__name" v-for="(element, i) in head" :key="i">
+          {{ element }}
+        </div>
+      </div>
+      <slot></slot>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  head: {
+    type: Array,
+    required: false,
+  },
+  columnTemplates: {
+    type: String,
+    required: false,
+  },
+});
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.table {
+  background-color: #fff;
+  width: 100%;
+  margin-bottom: 40px;
+  margin-top: 15px;
+  &-wrapper {
+    display: flex;
+    justify-content: center;
+  }
+  &-head {
+    padding: 5px 16px;
+    display: grid;
+    column-gap: 10px;
+    align-items: center;
+    border-bottom: 2px solid #eeeff4;
+    background: #fff;
+    &__name {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      color: #999;
+      cursor: pointer;
+    }
+    @media screen and (max-width: 767px) {
+      display: none;
+    }
+  }
+}
+</style>
--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,13 +1,69 @@
-<template>
-    <div>
-
-    </div>
-</template>
-
 <script setup>
+import { ref, computed } from "vue";
+import BaseTable from "@/components/Table/BaseTable.vue";
+import TableRow from "@/components/Table/TableRow.vue";
+import TableColumn from "@/components/Table/TableColumn.vue";
+import Button from "@/components/Button.vue";
 
+const tableHeads = ["Id", "Author", "Title", "Cover", ""];
+
+const sizeColumns = "50px 1fr 2fr 140px 150px";
+
+const books = ref([
+  {
+    id: 1,
+    author: "Dmitry Glukhovsky",
+    title: "Metro 2033",
+    image:
+      "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81pNKLAG-cL._AC_UY436_FMwebp_QL65_.jpg",
+    bg: "#FFA26B",
+  },
+  {
+    id: 12,
+    author: "James Clear",
+    title: "Atomic Habits: An Easy",
+    image: "https://m.media-amazon.com/images/P/0735211299.01._SCLZZZZZZZ_SX500_.jpg",
+    bg: "#00C48C",
+  },
+  {
+    id: 2,
+    author: "J. K. Rowling",
+    title: "Harry Potter and the Order of the Phoenix",
+    image:
+      "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51bZujlJxlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    bg: "#00C48C",
+  },
+]);
 </script>
 
-<style lang="scss" scoped>
+<template>
+  <h2 class="heading-1">Table</h2>
+  <BaseTable :head="tableHeads" :columnTemplates="sizeColumns">
+    <table-row
+      v-for="book in books"
+      :key="book.id"
+      :columnTemplates="sizeColumns"
+      :bgRow="book.bg"
+    >
+      <table-column :columnTitle="tableHeads[0]">
+        {{ book.id }}
+      </table-column>
 
-</style>
+      <table-column :columnTitle="tableHeads[1]">
+        {{ book.author }}
+      </table-column>
+
+      <table-column :columnTitle="tableHeads[2]">
+        {{ book.title }}
+      </table-column>
+
+      <TableColumn :image="true" :srcImage="book.image"> </TableColumn>
+
+      <table-column>
+        <Button lavel="Read Online"></Button>
+      </table-column>
+    </table-row>
+  </BaseTable>
+</template>
+
+<style lang="scss" scoped></style>
